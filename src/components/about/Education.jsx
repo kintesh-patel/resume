29bacf792a6798f9369e00442dfd81a2adb48c1b
Kintesh Patel <kintesh@goldenmace.com>
2024-04-26 23:38:07 +0530

    company name

--- src/components/about/Education.jsx
+++ src/components/about/Education.jsx
@@ -4,19 +4,19 @@ const experienceMain = [
   {
     year: "Communication",
     position: "Communication and Collaboration",
-    compnayName: "Goldenmace IT Solutions",
+    compnayName: "Innoquad Technologies",
     details: `Strong communication skills to understand client requirements and translate them into web applications. Ability to collaborate effectively with other team members, such as developers and project managers. Additionally, adept at handling client support tickets regarding various issues, ensuring timely resolution and client satisfaction.`,
   },
   {
     year: "Learn",
     position: " Continuous Learning",
-    compnayName: "Goldenmace IT Solutions",
+    compnayName: "Innoquad Technologies",
     details: `Eagerness to stay updated with the latest backend development trends, technologies, and tools.Willingness to learn new skills and adapt to evolving backend practices.`,
   },
   {
     year: "Work",
     position: "Portfolio",
-    compnayName: "Goldenmace IT Solutions",
+    compnayName: "Innoquad Technologies",
     details: `I have contributed to over 40 projects, including various critical assignments, showcasing my ability to handle high-pressure situations effectively. Demonstrated proficiency in building robust and efficient backend systems. Evidence of meticulous attention to detail in backend implementation, ensuring reliability and optimal performance across various platforms.`,
   },
 ];
--- src/components/about/Experience.jsx
+++ src/components/about/Experience.jsx
@@ -4,19 +4,19 @@ const experienceContent = [
   {
     year: "Web",
     position: " Web Development",
-    compnayName: "Goldenmace IT Solutions",
+    compnayName: "Innoquad Technologies",
     details: `Proficiently engineered and maintained robust backend solutions utilizing WordPress and PHP. Collaborated closely with backend development teams to ensure reliable integration and optimal performance of web applications.`,
   },
   {
     year: "Skill",
     position: "Skills",
-    compnayName: "Goldenmace IT Solutions",
+    compnayName: "Innoquad Technologies",
     details: `Experienced Backend Developer proficient in WordPress, Laravel, and PHP, with a track record of developing and maintaining robust web applications. Skilled in WordPress theme and plugin development, Laravel framework setup, routing, and database interactions. Possesses a strong understanding of PHP syntax, control structures, and security best practices. Collaborates effectively with cross-functional teams to ensure seamless integration and optimal performance of web solutions.`,
   },
   {
     year: "Management",
     position: " Time Management and Organization",
-    compnayName: "Goldenmace IT Solutions",
+    compnayName: "Innoquad Technologies",
     details: `Ability to manage multiple projects and meet deadlines.
     Strong organizational skills to maintain backend assets, files, and project documentation in a structured manner. Attention to detail to ensure precision and consistency in backend deliverables.`,
   },
